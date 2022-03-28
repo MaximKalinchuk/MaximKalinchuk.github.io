03ab2b372a3f6510282d1073195bd487a5f082ef
MaxMovement <kalinchuk.maxim@mail.ru>
2022-03-28 10:03:02 +0300

    first task

--- src/alert.js
+++ src/alert.js
@@ -1,2 +1,24 @@
 let thirstCounter = document.getElementsByClassName("counter")
-const item = thirstCounter[0].innerText
+
+let thirstCard = Number(thirstCounter[0].textContent)
+let secondCard = Number(thirstCounter[1].textContent)
+
+let id;
+const iter = () => {
+
+
+    if (thirstCard < 2) {
+        clearInterval(id)
+    }
+    console.log(thirstCard)
+    thirstCard -= 1
+    secondCard += 1
+
+    document.getElementsByClassName("counter")[0].innerHTML = `<img src="img/js-icon.svg"><h3>${thirstCard}</h3>`
+    document.getElementsByClassName("counter")[1].innerHTML = `<img src="img/js-icon.svg"><h3>${secondCard}</h3>`
+}
+
+id = setInterval(iter, 50)
+
+// const item = document.body.childNodes
+// console.log(item)
